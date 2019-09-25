5235c28a46ef005190f8671d055f7dc11994152b
3uxeca <sajihye4@gmail.com>
2019-09-25 17:18:39 +0900

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,12 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
 
 from django.urls import include, path
 from django.contrib import admin
@@ -26,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
